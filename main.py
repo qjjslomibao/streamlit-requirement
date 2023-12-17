311de0c26c4cfae746d1499bdee335a78fd23437
qjjslomibao <112042148+qjjslomibao@users.noreply.github.com>
2023-12-17 12:55:13 +0800

    Update main.py

--- main.py
+++ main.py
@@ -38,18 +38,31 @@ st.markdown(
         .white-text {
             color: #ffffff !important;
         }
+        .footer-text {
+            color: #ffffff !important;
+            font-style: italic;
+        }
     </style>
     """,
     unsafe_allow_html=True
 )
 
+# Text to display
+made_by_text = "This was made by:"
+submitted_to_text = "Submitted to Dr. Jonathan Taylor."
+
+# Display "This was made by:" with a blue background and white text
+st.markdown(f'<div class="highlight"><p class="big-font white-text">{made_by_text}</p></div>', unsafe_allow_html=True)
+
 # Names to display
 names = ["Lomibao, Justin Joshua", "Genabe, John Richmond", "Carl Voltair", "Lance Macamus", "Jimenez, Maw"]
 
 # Display names with a blue background and white text
-st.markdown('<div class="highlight"><p class="big-font white-text">Names:</p></div>', unsafe_allow_html=True)
 for name in names:
     st.markdown(f'<p class="big-font white-text">{name}</p>', unsafe_allow_html=True)
+
+# Display "Submitted to Dr. Jonathan Taylor." with a blue background and white text
+st.markdown(f'<p class="big-font white-text">{submitted_to_text}</p>', unsafe_allow_html=True)
 
 # File uploader widget to upload an image file
 uploaded_file = st.file_uploader("Choose an image...", type="jpg")
